52dcb8a6f5d6429ffa9fb72d1f3a10de540e0710
sovilleda07 <sovilleda07@gmail.com>
2026-03-15 17:01:01 -0600

    add: variant breadcrumb to Link

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -1,13 +1,19 @@
 import arrowLime from '../../assets/arrow-lime.png';
 import arrowPurple from '../../assets/arrow-purple.png';
+import { FaChevronLeft } from 'react-icons/fa6';
 
 type ArrowColor = 'lime' | 'purple';
+
+type LinkVariant = 'default' | 'breadcrumb';
 
 interface LinkProps {
   label?: string;
   onClick?: () => void;
   arrowColor?: ArrowColor;
   textColor?: string;
+  showArrow?: boolean;
+  variant?: LinkVariant;
+  iconLeft?: React.ReactNode;
 }
 
 const arrowAssets: Record<ArrowColor, string> = {
@@ -20,6 +26,9 @@ export const Link = ({
   onClick,
   arrowColor = 'lime',
   textColor = 'white',
+  showArrow = true,
+  variant = 'default',
+  iconLeft,
 }: LinkProps) => {
   const textColorClass =
     textColor === 'black'
@@ -28,17 +37,26 @@ export const Link = ({
         ? 'text-white'
         : '';
 
+  const shouldShowArrow = variant === 'breadcrumb' ? false : showArrow;
+  const defaultBreadcrumbIcon = (
+    <FaChevronLeft
+      className="w-[6px] h-[px] text-white"
+      aria-hidden="true"
+    />
+  );
+  const iconToRender =
+    variant === 'breadcrumb' ? (iconLeft ?? defaultBreadcrumbIcon) : iconLeft;
+
   return (
     <button
       onClick={onClick}
       className={`flex items-center gap-1 ${textColorClass} text-sm font-medium transition-colors cursor-pointer`}
     >
+      {iconToRender}
       {label}
-      <img
-        src={arrowAssets[arrowColor]}
-        alt="arrow"
-        className="w-[24px]"
-      />
+      {shouldShowArrow && (
+        <img src={arrowAssets[arrowColor]} alt="arrow" className="w-[24px]" />
+      )}
     </button>
   );
 };
