e655c4f154e10be6fb1e3602c6b4d45d30c70020
sovilleda07 <sovilleda07@gmail.com>
2026-03-14 20:35:39 -0600

    feat: configure router with Home and Post routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { Navbar } from './components/layout/Navbar';
-import { Footer } from './components/layout/Footer';
-
 function App() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar onNewPost={() => console.log('new post')} />
-      <main className="flex-1" />
-      <Footer />
-    </div>
-  );
+  return <div className="min-h-screen flex flex-col"></div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import './index.css';
+import App from './App.tsx';
+import { Post } from './pages/Post.tsx';
+import { Home } from './pages/Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<App />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
