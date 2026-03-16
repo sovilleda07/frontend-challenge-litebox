eacf9a01f2ef1e51677eb5535b833936f88704d8
sovilleda07 <sovilleda07@gmail.com>
2026-03-15 20:47:38 -0600

    refactor: update main file

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="min-h-screen flex flex-col"></div>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import './index.css';
-import App from './App.tsx';
-import { Post } from './pages/Post.tsx';
+import { PostPage } from './pages/Post.tsx';
 import { Home } from './pages/Home.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<App />} /> */}
         <Route path="/" element={<Home />} />
-        <Route path="/post/:id" element={<Post />} />
+        <Route path="/post/:id" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
